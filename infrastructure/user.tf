be4a9f1260cea90f3e3b3a7677b047d404cf0954
Gavan Lamb <gavan.jon.lamb@gmail.com>
2022-09-04 01:21:00 +1000

    Add user resources

---
+++ infrastructure/user.tf
@@ -0,0 +1,34 @@
+﻿variable "username" {
+  type = string
+}
+
+resource "aws_iam_user" "cicd" {
+  name = var.username
+  path = "/cicd/"
+  force_destroy = true
+  tags = local.default_tags
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+resource "aws_iam_access_key" "cicd" {
+  user = aws_iam_user.cicd.name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_iam_user_policy_attachment" "cicd_remote_state" {
+  user = aws_iam_user.cicd.name
+  policy_arn = module.backend.bucket_iam_policy_arn
+}
+resource "aws_iam_user_policy_attachment" "cicd_remote_lock" {
+  user = aws_iam_user.cicd.name
+  policy_arn = module.backend.dynamodb_iam_policy_arn
+}
+resource "aws_iam_user_policy_attachment" "cicd_remote_state_key" {
+  user = aws_iam_user.cicd.name
+  policy_arn = module.backend.kms_key_iam_policy_arn
+}
